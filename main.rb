62b165a6a7e23f3f9c86ca7c5f9d575cfb1b34e1
Mike Kelly <mike@mykanjo.co.uk>
2011-02-06 10:58:04 +0000

    update to default setup

--- main.rb
+++ main.rb
@@ -1,54 +1,46 @@
 run "rm -Rf README public/index.html public/javascripts/* test app/views/layouts/* app/helpers/*"
 
-gem "formtastic", "1.1" 
-gem 'flutie'
+gem "mongoid", "~>2.0"
+gem "bson_ext", "~>1.2"
 
+gem "flutie", "~>1.1"
 gem "devise"
-gem "cancan"
+#gem "cancan"
 
-#gem 'will_paginate', '>=3.0.pre'
-#gem 'friendly_id', '~>3.1'
+gem "jquery-rails"
 
-#gem 'rspec', '>=2.0.0.alpha.11', :group => :test
-#gem 'rspec-rails', '>=2.0.0.alpha.11', :group => :test
-#gem 'remarkable', '>=4.0.0.alpha4', :group => :test
-#gem "factory_girl_rails" # BUG => , :group => :test
+gem "rspec-rails", "~>2.4", :group => :development
 
-#gem 'cucumber', ">=0.6.3", :group => :cucumber
-#gem 'cucumber-rails', ">=0.3.2", :group => :cucumber
-#gem 'capybara', ">=0.3.6", :group => :cucumber
-#gem 'database_cleaner', ">=0.5.0", :group => :cucumber
+gem "rspec", :group =>:test
+gem "cucumber-rails", :group => :test
+gem "factory_girl_rails", :group => :test
+gem "capybara", :group => :test
+gem "database_cleaner", :group => :test
 
-#gem 'spork', ">=0.8.4", :group => :cucumber
-#gem "pickle", :git => "git://github.com/codegram/pickle.git", :group => :cucumber
-
-#gem 'inploy'
-
-#gem 'rails3-generators', :git => "git://github.com/indirect/rails3-generators.git"
-
-# application  <<-GENERATORS 
-# config.generators do |g|
-#   g.template_engine :erubis
-#   g.test_framework  :rspec, :fixture => true, :views => false
-#   g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
-# end
-# GENERATORS
 
 run "bundle install"
 
-#generate "rspec:install"
-#generate "cucumber:install --capybara --rspec" # --spork"
-#generate "friendly_id"
+application  <<-GENERATORS
+  config.generators do |g|
+    g.template_engine :erb
+    g.orm :mongoid
+  end
+GENERATORS
 
-generate "formtastic:install"
+generate "mongoid:config"
+
+generate "rspec:install"
+generate "cucumber:install --capybara --rspec"
+
+generate "jquery:install"
+
+generate "devise:install"
+
 run "rake flutie:install"
 
-get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 get "https://github.com/mikekelly/rails3_template/raw/master/screen.css" ,"public/stylesheets/screen.css"
 get "https://github.com/mikekelly/rails3_template/raw/master/application.html.erb" ,"app/views/layouts/application.html.erb"
 get "https://github.com/mikekelly/rails3_template/raw/master/application_helper.rb" ,"app/helpers/application_helper.rb" 
-#get "https://github.com/mikekelly/infoether-framework/raw/master/public/stylesheets" ,"app/views/layouts/application.html.erb"  
-#get "https://github.com/dcrec1/rails3_template/raw/master/factory_girl.rb", "features/support/factory_girl.rb"
 
 git :init
 git :add => '.'
